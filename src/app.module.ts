a389bd7ae53c492155666079d1e987614be6b048
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-03-22 18:11:04 +0530

    chore(setup): make structure with core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CoreModule],
+  controllers: [],
 })
 export class AppModule {}
