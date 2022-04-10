43b6484649dba8013166a05279df9e61208acf10
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-04-10 18:00:18 +0530

    chore(package): add bcrypt types

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto, SignupDto } from './dto';
 import { PrismaService } from 'nestjs-prisma';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signUp(signupDto: SignupDto) {
-    console.log({ signupDto });
-    // return this.prisma.user.create(signupDto);
+  async signUp(signupDto: SignupDto) {
+    const hashedPassword = await bcrypt.hash(signupDto.password, 10);
+    console.log({ hashedPassword });
+    const user = { ...signupDto, password: hashedPassword };
+    // return this.prisma.user.create(user);
     return 'signup';
   }
 
