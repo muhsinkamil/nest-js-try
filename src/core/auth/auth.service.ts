0f17138cadee7d3a19d7e31571b7130fcc7090d3
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-04-17 17:59:14 +0530

    feat(login): add login comparison

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,34 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { LoginDto, SignupDto } from './dto';
 import { PrismaService } from 'nestjs-prisma';
 import * as bcrypt from 'bcrypt';
+import { isInstance } from 'class-validator';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signUp(signupDto: SignupDto) {
-    const hashedPassword = await bcrypt.hash(signupDto.password, 10);
-    const user = { ...signupDto, password: hashedPassword };
-    const savedUser = this.prisma.user.create({
-      data: {
-        email: user.email,
-        password: hashedPassword,
-        firstName: user.firstName,
-        lastName: user.lastName,
-      },
-    });
+    try {
+      const hashedPassword = await bcrypt.hash(signupDto.password, 10);
+      const user = { ...signupDto, password: hashedPassword };
+      const savedUser = await this.prisma.user.create({
+        data: {
+          email: user.email,
+          password: hashedPassword,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        },
+      });
 
-    return savedUser;
+      delete savedUser.password;
+
+      return savedUser;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already exists');
+        }
+      }
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  login(loginDto: LoginDto) {
-    // const savedUser = this.prisma.user.findUnique({
-    //   where: {
-    //     email: loginDto.email,
-    //   },
-    // });
-    // bcrypt.compare(savedUser, loginDto.password);
-    return 'login';
+  async login(loginDto: LoginDto) {
+    try {
+      const savedUser = await this.prisma.user.findUnique({
+        where: {
+          email: loginDto.email,
+        },
+      });
+
+      if (!savedUser) {
+        throw new ForbiddenException('Credentials mismatch');
+      }
+
+      const isPasswordMatch = await bcrypt.compare(
+        loginDto.password,
+        savedUser.password,
+      );
+
+      if (!isPasswordMatch) {
+        throw new ForbiddenException('Credentials mismatch');
+      }
+
+      delete savedUser.password;
+
+      return savedUser;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }
